a8ed6a23a08cb42e34052943c7bf9986915a6eb5
Sashko Stubailo <sashko@meteor.com>
2018-03-13 15:17:43 -0700

    Prettier

--- src/schema.js
+++ src/schema.js
@@ -1,15 +1,17 @@
-const { makeExecutableSchema } = require('graphql-tools');
+const { makeExecutableSchema } = require("graphql-tools");
 
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
+
+const gql = String.raw;
 
 // Construct a schema, using GraphQL schema language
-const typeDefs = `
+const typeDefs = gql`
   type Query {
     myFavoriteArtists: [Artist]
   }
 
   type Artist @cacheControl(maxAge: 60) {
-		id: ID
+    id: ID
     name: String
     image: String
     twitterUrl: String
@@ -26,55 +28,64 @@ const typeDefs = `
 const resolvers = {
   Query: {
     myFavoriteArtists: (root, args, context) => {
-      return Promise.all(myFavoriteArtists.map(({name, id}) => {
-        return fetch(`https://app.ticketmaster.com/discovery/v2/attractions/${id}.json?apikey=${context.secrets.TM_API_KEY}`)
-          .then(res => res.json())
-          .then(data => {
-          	return Object.assign({ name, id }, data);
-          });
-      }));
+      return Promise.all(
+        myFavoriteArtists.map(({ name, id }) => {
+          return fetch(
+            `https://app.ticketmaster.com/discovery/v2/attractions/${id}.json?apikey=${
+              context.secrets.TM_API_KEY
+            }`
+          )
+            .then(res => res.json())
+            .then(data => {
+              return Object.assign({ name, id }, data);
+            });
+        })
+      );
     }
   },
   Artist: {
-  	twitterUrl: (artist) => {
+    twitterUrl: artist => {
       return artist.externalLinks.twitter[0].url;
     },
-    image: (artist) => artist.images[0].url,
+    image: artist => artist.images[0].url,
     events: (artist, args, context) => {
-      return fetch(`https://app.ticketmaster.com/discovery/v2/events.json?size=10&apikey=${context.secrets.TM_API_KEY}&attractionId=${artist.id}`)
+      return fetch(
+        `https://app.ticketmaster.com/discovery/v2/events.json?size=10&apikey=${
+          context.secrets.TM_API_KEY
+        }&attractionId=${artist.id}`
+      )
         .then(res => res.json())
         .then(data => {
           // Sometimes, there are no upcoming events
-        	return (data && data._embedded && data._embedded.events) || []
-      	});
-    },
-
-	},
+          return (data && data._embedded && data._embedded.events) || [];
+        });
+    }
+  },
   Event: {
-    image: (event) => event.images[0].url,
-    startDateTime: (event) => event.dates.start.dateTime
+    image: event => event.images[0].url,
+    startDateTime: event => event.dates.start.dateTime
   }
-}
+};
 
 // Required: Export the GraphQL.js schema object as "schema"
 const schema = makeExecutableSchema({
   typeDefs,
-  resolvers,
+  resolvers
 });
 
 const myFavoriteArtists = [
   {
-    name: 'Kansas',
-    id: 'K8vZ9171C-f',
+    name: "Kansas",
+    id: "K8vZ9171C-f"
   },
   {
-    name: 'Lil Yachty',
-    id: 'K8vZ9174v57',
+    name: "Lil Yachty",
+    id: "K8vZ9174v57"
   },
   {
-    name: 'Jason Mraz',
-    id: 'K8vZ9171CVV',
-  },
+    name: "Jason Mraz",
+    id: "K8vZ9171CVV"
+  }
 ];
 
 module.exports = { schema };
